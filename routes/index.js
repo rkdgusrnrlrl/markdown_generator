4a9a27250a57085a28215fc34a7c8d5a08b8c465
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-03-29 18:39:02 +0900

    dbox 라이브러리설치, dbox 라이브러리 테스트 중

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 var express = require('express');
-var Dropbox = require("dropbox");
+var dbox  = require("dbox")
 var router = express.Router();
+var app   = dbox.app({ "app_key": "06r6pi3eyiazdm3", "app_secret": "06r6pi3eyiazdm3" })
 
 
-var client = new Dropbox.Client({
-    key: "your-key-here",
-    secret: "your-secret-here"
-});
+
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-    var client = app.client("lX5-zkIFJGUAAAAAAAB1nueg3II0vRur31drgj-c4CFdEWAktrWiFLtHmvsw2Hb5");
-
-    client.account(function(status, reply){
-        console.log(status);
-        console.log(reply);
-    });
-  //res.render('index', { title: 'Express' });
+    //var client = app.client("lX5-zkIFJGUAAAAAAAB1uBPShfsJVR08xe1HpleNc9llr3xqxElM_QyY0vfo8xAI");
+    app.requesttoken(function(status, request_token){
+        console.log(request_token)
+    })
 });
-
 module.exports = router;
+
