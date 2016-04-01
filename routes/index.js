a42177f10f4f166c59a88ec38be7543000398c9b
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-04-01 14:42:40 +0900

    css 경로는 렌더링할 때 정하게 수정 index.js 리펙토링 및 코드 정리

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,9 @@ var marked = require('marked');
 
 var appKey = '06r6pi3eyiazdm3'
 var appSecret = 'aeunwyrz0lxrk0u'
-
 var access_tocken = 'lX5-zkIFJGUAAAAAAAB1v-bmjTXqxmA3ZZ3JZMqwbu9GyHp4SkJiz6zn0t7_q-mk';
 
-
-
-
-var optionsForAccountInfo = {
-    hostname: 'api.dropboxapi.com',
-    path: '/2/users/get_current_account',
-    port: 443,
-    method: 'POST',
-    headers: {
-        'Authorization': 'Bearer lX5-zkIFJGUAAAAAAAB1v-bmjTXqxmA3ZZ3JZMqwbu9GyHp4SkJiz6zn0t7_q-mk'
-    }
-};
-
+//파일 다운로드를 위한 request option
 var optionsForDownloadMd = {
     hostname: 'content.dropboxapi.com',
     path: '/2/files/download',
@@ -32,46 +19,47 @@ var optionsForDownloadMd = {
     }
 };
 
-router.get('/getAccount', function(reqs, resp, next) {
+/**
+ * dropbox api 가 error를 json 형태로 보내면
+ * error_summary 파악해 error 메세지를 리턴
+ * @param data
+ */
+function errorhandler(data) {
+    var jsonData = JSON.parse(data);
 
-    var req = https.request(optionsForAccountInfo, function (res) {
-        res.on('data', function (chunk) {
-            console.log('what is junk');
-            console.log('BODY: '+chunk);
-        });
-        res.on('end', function () {
-            console.log('response end');
-            resp.send()
-        })
-    }).end();
+    var error_msg = jsonData['error_summary'];
 
-});
-
-function getHtmlBody(res, data) {
-    var body = "";
-    if (res.headers['content-type'] == 'application/json') { //error 핸들링
-        var jsonData = JSON.parse(data);
-
-        var error_msg = jsonData['error_summary'];
-        if (error_msg.includes("path/not_found/") > -1) {
-            var body = "해당경로에 파일이 존재하지 않습니다.";
-        }
-    } else {
-        var body = marked('' + data);
+    if (error_msg.includes("path/not_found/") > -1) {
+        return "해당경로에 파일이 존재하지 않습니다.";
     }
-    return body;
+
+    return "에러입니다.";
+}
+
+
+/**
+ * respon 을 체크해 application/json 이면 에러로 간추 에러 처리를 함
+ * @param res
+ * @param data
+ * @returns {string}
+ */
+function getHtmlBody(res, data) {
+    if (res.headers['content-type'] == 'application/json') { //error 핸들링
+        return errorhandler(data);
+    } else {
+        return marked('' + data);
+    }
 }
 
 router.get('/Documents/*', function(reqs, resp, next) {
-    var filePath = reqs.originalUrl;
-    var path = {"path": filePath };
-    console.log("filePath : "+filePath);
+    var path = {"path": reqs.originalUrl };
     optionsForDownloadMd.headers['Dropbox-API-Arg'] = JSON.stringify(path);
+
     var req = https.request(optionsForDownloadMd, function (res) {
         res.on('data', function (data) {
             var body = getHtmlBody(res, data);
             console.log("body : "+body);
-            resp.render('index', {body : body})
+            resp.render('index', {body : body, css : "/css/my_style.css"})
         });
     }).end();
 
