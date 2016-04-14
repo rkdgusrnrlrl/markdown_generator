44ccb50faee386d9f25a5f127c099af7e34b1584
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-04-14 15:34:48 +0900

    Deploy 폴더를 만들어 관리함 Docker Image 빌드시 Deploy 파일과 깃파일을 제거함 이미지 존재시 다시 빌드하지 않음 컨테이너 존재시 컨테이너 삭제하고 run

--- deploy/docker_run_shell.sh
+++ deploy/docker_run_shell.sh
@@ -3,5 +3,9 @@ if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
   docker build -t mark_img:0.1 .
 fi
 
+if [[ "$(docker ps | grep mark_cont 1> /dev/null)" == "" ]]; then
+  docker rm -f mark_cont
+fi
+
 docker run -itd --name mark_cont -p 81:3000 mark_img:0.1 bash
 docker exec mark_cont forever start /data/bin/www
