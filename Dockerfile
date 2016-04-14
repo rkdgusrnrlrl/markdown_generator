2ce73b86b32ed5e717d19fb61b66ba7daa421af7
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-04-14 11:57:50 +0900

    container run 하는 쉘 스크립트 검증됨 다만 accese_tocken을 받아서 파일로 저장하는 기능 추가 필요

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN mkdir /data
 WORKDIR /data
 
 COPY ./ /data/
+RUN rm -rf /data/Dockerfile /data/docker_run_shell.sh /data/.git /data/
 RUN npm intall forever -g
 EXPOSE 3000
 
--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker build --tage mark_img:0.1
-docker run -itd -p 81:3000 --name mark_con mark_img:0.1
+if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
+  docker build -t mark_img:0.1 .
+fi
+
+docker run -itd --name mark_cont -p 81:3000 mark_img:0.1 bash
+docker exec mark_cont forever start /data/bin/www
