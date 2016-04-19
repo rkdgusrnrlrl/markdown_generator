7cb1e1fd9eb40881a1589a3f0176bec071f82303
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-04-19 12:07:42 +0900

    컨테이너 유무 체크하는 조건식이 잘못되어 수정함

--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -3,9 +3,10 @@ if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
   docker build -t mark_img:0.1 .
 fi
 
-if [[ "$(docker ps | grep mark_cont 2> /dev/null)" == "" ]]; then
+if [[ "$(docker ps | grep mark_cont 2> /dev/null)" != "" ]]; then
   docker rm -f mark_cont
 fi
 
 docker run -itd --name mark_cont -p 81:3000 mark_img:0.1 bash
 docker exec mark_cont forever start /data/bin/www
+docke
