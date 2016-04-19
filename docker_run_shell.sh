d3da7864c95d6275dbaf3bb9211ad9b6584a63cc
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-04-19 12:00:04 +0900

    docker run 하는 부분 외의 것은 삭제함

--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-while (true); do
-    echo "access_token 을 입력해 주세요"
-    read ACCESS_TOKEN
-    curl -X POST https://api.dropboxapi.com/2/users/get_current_account \
-        --header 'Authorization: Bearer ${ACCESS_TOKEN}' \
-        --header 'Content-Type: application/json' \
-        --data 'null' 2> /dev/null
-    OAUTH_ACCESS_TOKEN_SECRET=$(sed -n 's/oauth_token_secret=\([a-z A-Z 0-9]*\)&.*/\1/p' "$RESPONSE_FILE")
-
-done
-
 if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
   docker build -t mark_img:0.1 .
 fi
