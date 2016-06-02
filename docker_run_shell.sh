8de798cfceb5118cc2a3eeaeb628d4d1983612e4
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-02 09:46:00 +0900

    docker run 할 때 public 폴더의 파일과 컨테이너의 폴더를 맵핑 시킴

--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Docker 빌드를 시작합니다."
+DIR=$(pwd)
 if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
   docker build -t mark_img:0.1 .
 fi
@@ -8,5 +9,5 @@ if [[ "$(docker ps | grep mark_cont 2> /dev/null)" != "" ]]; then
   docker rm -f mark_cont
 fi
 
-docker run -itd --name mark_cont -p 81:3000 mark_img:0.1 bash
+docker run -itd --name mark_cont -v ${DIR}/public:/data/public -p 81:3000 mark_img:0.1 bash
 docker exec mark_cont forever start /data/bin/www
