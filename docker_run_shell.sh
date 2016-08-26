8f1d2eaf4fbdfb0e520199d70b9ed81cb57d28b0
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-08-26 14:29:02 +0900

    forever 를 바로 실행 시킬수 있게 하기 위해서 DockerFile 변경해 새로운 이미지 사용

--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Docker 빌드를 시작합니다."
 DIR=$(pwd)
-if [[ "$(docker images -q mark_img:0.1 2> /dev/null)" == "" ]]; then
-  docker build -t mark_img:0.1 .
+if [[ "$(docker images -q mark_img:0.2 2> /dev/null)" == "" ]]; then
+  docker build -t mark_img:0.2 .
 fi
 
 if [[ "$(docker ps -a | grep mark_cont 2> /dev/null)" != "" ]]; then
   docker rm -f mark_cont
 fi
 
-docker run -itd --name mark_cont -v ${DIR}/public:/data/public -p 81:3000 mark_img:0.1
+docker run -itd --name mark_cont -v ${DIR}/public:/data/public -p 81:3000 mark_img:0.1 b
 
