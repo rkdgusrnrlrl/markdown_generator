1bd90023171b798e9e29a4e08f7b565ae4098995
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-08-26 13:20:10 +0900

    한글 처리 테스트 작성 및 전체 리스트 추출 메서드 작성

--- test/unit.js
+++ test/unit.js
@@ -8,6 +8,7 @@ var chaiAsPromised = require("chai-as-promised");
 var expect = chai.expect;
 var assert = chai.assert;
 var dropbox = require('../lib/drop.js')();
+var resource = require('../resource/resource.json');
 
 chai.should()
 chai.use(chaiAsPromised);
@@ -71,7 +72,7 @@ describe("if empty mark file", function() {
     }
 
     it("should be show empty", function () {
-        var filename = "/WorkFlow.md"
+        var filename = "/WorkFlow.md";
         return dropbox.getMetaData(filename)
             .then((fileMetaData) => {
                 if (fileMetaData.size == 0) {
@@ -87,8 +88,127 @@ describe("if empty mark file", function() {
                 assert.equal(markdown, "")
             })
     })
-})
+});
 
+describe("한글파일명 처리", function() {
+    var https = require('https')
+    var option = {
+        hostname: 'api.dropboxapi.com',
+        path: '/2/files/get_metadata',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    var optionDown = {
+        hostname: 'content.dropboxapi.com',
+        path: '/2/files/download',
+        port: 443,
+        method: 'POST',
+        headers: {
+
+        }
+    };
+
+    it("메타데이터 가져오기", function () {
+        dropbox.addAuthorizationToHeader(option, resource.accessTocken);
+        function makePathJsonString(filename) {
+            var postData = {path: ""};
+            postData['path'] = filename;
+            return JSON.stringify(postData);
+        }
+
+        var getList = new Promise((fulfil, reject) => {
+            var metaReq = https.request(option, function (resForFileList) {
+                var data = "";
+                resForFileList.on('data',  (val) => data += val+"" );
+                resForFileList.on('end',  (val) => fulfil(data) );
+            });
+            metaReq.write(makePathJsonString("/long-pulling 방식의 서버 개발.md"));
+            metaReq.end();
+        });
+        return getList.then((json) => console.log(json));
+    });
+
+
+    it("다운로드", function () {
+        dropbox.addAuthorizationToHeader(optionDown, resource.accessTocken);
+
+        function unicodeEscape(str){
+            var result = "";
+            for(var i = 0; i < str.length; i++){
+                result += "\\u" + ("000" + str[i].charCodeAt(0).toString(16)).substr(-4);
+            }
+            return result;
+        };
+        var fileNameEnc= unicodeEscape("/long-pulling 방식의 서버 개발.md");
+        console.log(encodeURI("/long-pulling 방식의 서버 개발.md"));
+        console.log(encodeURIComponent("/long-pulling 방식의 서버 개발.md"));
+        console.log(fileNameEnc);
+
+
+        optionDown.headers['Dropbox-API-Arg'] = "{ \"path\" : \""+fileNameEnc+"\" }";
+        function makePathJsonString(filename) {
+            var postData = {path: ""};
+            postData['path'] = filename;
+            return JSON.stringify(postData);
+        }
+
+        var getList = new Promise((fulfil, reject) => {
+            var metaReq = https.request(optionDown, function (resForFileList) {
+                var data = "";
+                resForFileList.on('data',  (val) => data += val+"" );
+                resForFileList.on('end',  (val) => fulfil(data) );
+            });
+            metaReq.end();
+        });
+        return getList.then((json) => console.log(json));
+    });
+
+
+});
+
+
+
+describe("전체 리스트 가져오는 코드", function() {
+    var https = require('https')
+    var option = {
+        hostname: 'api.dropboxapi.com',
+        path: '/2/files/list_folder',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+
+    it("성공 기원", function () {
+        dropbox.addAuthorizationToHeader(option, resource.accessTocken);
+        function makePathJsonString(filename) {
+            var postData = {path: ""};
+            postData['path'] = filename;
+            return JSON.stringify(postData);
+        }
+
+        var getList = new Promise((fulfil, reject) => {
+            var metaReq = https.request(option, function (resForFileList) {
+                var data = "";
+                resForFileList.on('data',  (val) => data += val+"" );
+                resForFileList.on('end',  (val) => fulfil(data) );
+            });
+            metaReq.write(makePathJsonString(""));
+            metaReq.end();
+        });
+        return getList.then((json) => {
+            var allFileList = JSON.parse(json).entries;
+            allFileList.forEach((val)=>{
+                console.log(val.name);
+            })
+        });
+    })
+});
 
 
 
