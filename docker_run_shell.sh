ce0c9cca02399cd5db94569b1fce3e3afdf745ca
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2017-01-12 13:07:47 +0900

    title html unescaping docker 빌드 포트 변경 html-entities 라이브러리 추가

--- docker_run_shell.sh
+++ docker_run_shell.sh
@@ -9,5 +9,5 @@ if [[ "$(docker ps -a | grep mark_cont 2> /dev/null)" != "" ]]; then
   docker rm -f mark_cont
 fi
 
-docker run -itd --name mark_cont -v ${DIR}/public:/data/public -p 81:3000 mark_img:0.3 forever /data/bin/www
+docker run -itd --name mark_cont -v ${DIR}/public:/data/public -p 80:3000 mark_img:0.3 forever /data/bin/www
 
