b174edcf73892879855b4887669bbf6897e1ca80
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-10 23:37:39 +0900

    refec:refectoring unit test

--- test/unit.js
+++ test/unit.js
@@ -9,215 +9,84 @@ var expect = chai.expect;
 var assert = chai.assert;
 var dropbox = require('../lib/drop.js')();
 var resource = require('../resource/resource.json');
-
-chai.should()
+const rq = require('request-promise-native');
+const util = require('../lib/util');
+const _ = require('lodash');
+chai.should();
 chai.use(chaiAsPromised);
 
-//파일메타데이터 가져오는 프로미스 테스트
-describe("Test getMeatData promise code", function(){
-    it("should be contains Docker.md", function() {
-        return dropbox.getMetaData("/Docker.md")
-            .then((fileMeta) => {
-                expect(fileMeta['name']).equal("Docker.md")
-            })
-    });
-});
-
 //파일메타데이터 가져오는 프로미스 테스트 : 잘못된 데이터
-describe("Test getMeatData promise code : put wrong filename", function(){
-    it("should be error", function() {
-        dropbox.getMetaData("/asdfasdf")
-            .then(null, (err) => {
-                assert.ifError(err)
-            })
+describe("메타데이터 가져오기", function(){
+    it("파일메타데이터 가져오는 프로미스 테스트 Docker.md", async () => {
+        const fileMeta = await dropbox.getMetaData("/Docker.md");
+        expect(fileMeta['name']).equal("Docker.md");
+    }).timeout(10000);
+
+    it("없는 파일을 찾을 경우  에러 발생", async () => {
+        expect(dropbox.getMetaData("/asdfasdf")).to.be.rejectedWith(Error, "Error: path not_found")
     });
+
+    it("파일정보 체크후 마크다운 파일 가져오기", async () => {
+        const filename = "/Docker.md";
+        await dropbox.getMetaData(filename);
+        const markdown = await dropbox.downMarkDown(filename);
+        assert.include(markdown, "Docker");
+    }).timeout(10000)
 });
 
 //마크다운 파일 가져오는지 테스트
-describe("get markdown file", function() {
-    it("should be get mark", function () {
-        var filename = "/Docker.md"
+describe("마크다운 파일 다운로드", function() {
+    it("마크다운 파일 가져오는지 테스트", function () {
+        const filename = "/Docker.md";
         return dropbox.downMarkDown(filename)
             .then((val) => {
                 assert.include(val, "Docker")
             })
-    })
-})
-
-//파일정보 체크후 마크다운 파일 가져오기
-describe("if meta success get mark", function() {
-    it("should be get mark", function () {
-        var filename = "/Docker.md"
-        return dropbox.getMetaData(filename)
-            .then(() => {
-                return dropbox.downMarkDown(filename)
-            })
-            .then((markdown) => {
-                assert.include(markdown, "Docker")
-            })
-    })
-})
-
-//파일에 내용이 없는 경우 무한 루프 도는 것을 방지.
-//curl의 경우 문제가 없느나 node의 https request의 경우 계속 돌음
-describe("if empty mark file", function() {
-
-    function saveMdFile(jsonMeta, markdownContents, markDownDatas) {
-        markDownDatas[jsonMeta.name] = {
-            name: jsonMeta.name,
-            rev: jsonMeta.rev,
-            contents: markdownContents
-        }
-        return markdownContents;
-    }
-
-    it("should be show empty", function () {
-        var filename = "/WorkFlow.md";
-        return dropbox.getMetaData(filename)
-            .then((fileMetaData) => {
-                if (fileMetaData.size == 0) {
-                    return saveMdFile(fileMetaData, "", markDownDatas)
-                } else {
-                    return dropbox.downMarkDown("/"+fileMetaData.name)
-                        .then((markdownfile) => {
-                            return saveMdFile(fileMetaData, markdownfile, markDownDatas)
-                        });
-                }
-            })
-            .then((markdown) => {
-                assert.equal(markdown, "")
-            })
-    })
+    }).timeout(10000)
 });
 
 describe("한글파일명 처리", function() {
-    var https = require('https')
-    var option = {
-        hostname: 'api.dropboxapi.com',
-        path: '/2/files/get_metadata',
-        port: 443,
-        method: 'POST',
+
+    let requestOption = {
+        url: 'https://api.dropboxapi.com/2/files/get_metadata',
         headers: {
             'Content-Type': 'application/json'
-        }
-    };
-    var optionDown = {
-        hostname: 'content.dropboxapi.com',
-        path: '/2/files/download',
-        port: 443,
-        method: 'POST',
-        headers: {
-
-        }
+        },
+        json : true,
+        body : {}
     };
 
-    it("메타데이터 가져오기", function () {
-        dropbox.addAuthorizationToHeader(option, resource.accessTocken);
-        function makePathJsonString(filename) {
-            var postData = {path: ""};
-            postData['path'] = filename;
-            return JSON.stringify(postData);
-        }
+    it("한글 파일명 메타데이터 가져오기", async () => {
+        dropbox.addAuthorizationToHeader(requestOption, resource.accessTocken);
+        const fileName = "long-pulling 방식의 서버 개발.md";
+        requestOption.body = { path : `/${fileName}` };
 
-        var getList = new Promise((fulfil, reject) => {
-            var metaReq = https.request(option, function (resForFileList) {
-                var data = "";
-                resForFileList.on('data',  (val) => data += val+"" );
-                resForFileList.on('end',  (val) => fulfil(data) );
-            });
-            metaReq.write(makePathJsonString("/long-pulling 방식의 서버 개발.md"));
-            metaReq.end();
-        });
-        return getList.then((json) => console.log(json));
+        const respData = await rq.post(requestOption);
+        expect(respData.name).to.equal(fileName);
+    }).timeout(10000);
+
+    let requestOptionDown = {
+        url: 'https://content.dropboxapi.com/2/files/download',
+        headers: {}
+    };
+
+    it("다운로드", async () => {
+        dropbox.addAuthorizationToHeader(requestOptionDown, resource.accessTocken);
+        const fileNameEnc= util.unicodeEscape("/long-pulling 방식의 서버 개발.md");
+        requestOptionDown.headers['Dropbox-API-Arg'] = "{ \"path\" : \""+fileNameEnc+"\" }";
+        const respData = await rq.post(requestOptionDown);
+
+        expect(respData).to.include("# long-pulling 방식의 서버 개발 #");
     });
-
-
-    it("다운로드", function () {
-        dropbox.addAuthorizationToHeader(optionDown, resource.accessTocken);
-
-        function unicodeEscape(str){
-            var result = "";
-            for(var i = 0; i < str.length; i++){
-                result += "\\u" + ("000" + str[i].charCodeAt(0).toString(16)).substr(-4);
-            }
-            return result;
-        };
-        var fileNameEnc= unicodeEscape("/long-pulling 방식의 서버 개발.md");
-        console.log(encodeURI("/long-pulling 방식의 서버 개발.md"));
-        console.log(encodeURIComponent("/long-pulling 방식의 서버 개발.md"));
-        console.log(fileNameEnc);
-
-
-        optionDown.headers['Dropbox-API-Arg'] = "{ \"path\" : \""+fileNameEnc+"\" }";
-        function makePathJsonString(filename) {
-            var postData = {path: ""};
-            postData['path'] = filename;
-            return JSON.stringify(postData);
-        }
-
-        var getList = new Promise((fulfil, reject) => {
-            var metaReq = https.request(optionDown, function (resForFileList) {
-                var data = "";
-                resForFileList.on('data',  (val) => data += val+"" );
-                resForFileList.on('end',  (val) => fulfil(data) );
-            });
-            metaReq.end();
-        });
-        return getList.then((json) => console.log(json));
-    });
-
-
 });
 
-
-
 describe("전체 리스트 가져오는 코드", function() {
-    it("dropbox-api 사용", function () {
-        return dropbox.getAllFileList()
-            .then((json) => {
-                var allFileList = JSON.parse(json).entries;
-                allFileList.forEach((val)=> {
-                    console.log(val.name);
-                })
-            });
+    it("dropbox-api 사용", async () => {
+        const data = await dropbox.getAllFileList();
+        const allFileList = JSON.parse(data).entries;
+        const fileNamea = _.map(allFileList, val => val.name);
+        expect(fileNamea).to.include("how_develop_long-pulling_server.md");
     });
-
-    var https = require('https')
-    var option = {
-        hostname: 'api.dropboxapi.com',
-        path: '/2/files/list_folder',
-        port: 443,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-
-    it("성공 기원", function () {
-        dropbox.addAuthorizationToHeader(option, resource.accessTocken);
-        function makePathJsonString(filename) {
-            var postData = {path: ""};
-            postData['path'] = filename;
-            return JSON.stringify(postData);
-        }
-
-        var getList = new Promise((fulfil, reject) => {
-            var metaReq = https.request(option, function (resForFileList) {
-                var data = "";
-                resForFileList.on('data',  (val) => data += val+"" );
-                resForFileList.on('end',  (val) => fulfil(data) );
-            });
-            metaReq.write(makePathJsonString(""));
-            metaReq.end();
-        });
-        return getList.then((json) => {
-            var allFileList = JSON.parse(json).entries;
-            allFileList.forEach((val)=>{
-                console.log(val.name);
-            })
-        });
-    })
 });
 
 
